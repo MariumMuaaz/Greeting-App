ba020d9b68c0e9b497f83ee126585efe44efc0f2
Marium Muaaz <mariummuaaz@gmail.com>
2023-03-15 23:54:57 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 // import logo from './logo.svg';
 import './App.css'; 
-import  './components/style.css';
+import greeting from './components/greeting'; 
+
 
 function App() {  
-  let curDate = new Date(2023, 5, 5, 13);
-  curDate = curDate.getHours(); 
-  let greeting = " " ;  
-  const cssStyle = { };
 
-  if (curDate >=1 && curDate <12){  
-    greeting = 'Good Morning';    
-    cssStyle.color = 'green';
-  }else if (curDate >= 12 && curDate < 19)  { 
-    greeting = 'Good Afternoon';  
-    cssStyle.color = 'blue';
-  }else { 
-    greeting = 'Good Night';  
-    cssStyle.color = 'black';
-  }
   return (
     <div className="App">
-    <h1>Hello Sir, <span style={cssStyle}> {greeting} </span> </h1>
+    <greeting></greeting>   
     </div>
   );
 }
